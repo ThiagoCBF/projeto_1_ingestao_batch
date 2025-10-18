13cd563392bb8a860dc4156b2e6ce975ef3cbd6b
Thiago Ferreira <thiagocbf@outlook.com>
2025-10-18 11:39:57 -0300

    Criação dos scripts de ingestão e requirements.txt

---
+++ scripts/ingest_bronze_to_silver.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from datetime import datetime, timedelta
+from pathlib import Path
+import os
+import pyspark.sql.functions as F
+
+DATALAKE_PATH = '/home/thiagoferreira/Área de trabalho/portfolio/datalake'
+
+import os
+from pyspark.sql import SparkSession
+from delta import *
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
+os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
+
+builder = SparkSession.builder.appName("TesteDelta") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+print("SparkSession criada com sucesso!")
+print("Versão do Spark:", spark.version)
+
+def ingest_bronze_to_silver() -> None:
+
+    BRONZE_PATH = f"{DATALAKE_PATH}/bronze/transaction_data"
+    SILVER_PATH = f"{DATALAKE_PATH}/silver/transaction_data"
+
+    df_bronze = DeltaTable.forPath(spark, BRONZE_PATH)
+
+    try:
+        df_silver = DeltaTable.forPath(spark, SILVER_PATH)
+        df_silver.toDF().limit(1)
+
+        MAIOR_DATA_SILVER = (
+            df_silver
+            .toDF()
+            .agg(
+                F.max('transaction_date')
+            )
+            .collect()[0][0]
+        )
+
+        df_bronze = df_bronze.toDF().filter(f"transaction_date > '{MAIOR_DATA_SILVER}'")
+        
+        (
+            df_silver.alias('old_data')
+            .merge(
+                df_bronze.alias('new_data'),
+                "old_data.transaction_id = new_data.transaction_id"
+            )
+            .whenMatchedUpdateAll()
+            .whenNotMatchedInsertAll()
+            .execute()
+        )
+
+    
+    except Exception as e:
+        if 'DELTA_MISSING_DELTA_TABLE' in str(e):
+            (
+                df_bronze
+                .toDF()
+                .write
+                .format('delta')
+                .option('mergeSchema', 'true')
+                .mode('overwrite')
+                .save(SILVER_PATH)
+            )
+        else:
+            raise e
---
+++ scripts/ingest_source_to_bronze.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from datetime import datetime, timedelta
+from pathlib import Path
+import os
+import pyspark.sql.functions as F
+
+DATALAKE_PATH = '/home/thiagoferreira/Área de trabalho/portfolio/datalake'
+
+import os
+from pyspark.sql import SparkSession
+from delta import *
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
+os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
+
+builder = SparkSession.builder.appName("TesteDelta") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+print("SparkSession criada com sucesso!")
+print("Versão do Spark:", spark.version)
+
+
+def ingest_source_to_bronze(data_processamento:str) -> None:
+
+    SOURCE_PATH = f'{DATALAKE_PATH}/source/transaction_system/transaction_data_{data_processamento.replace('-', '_')}.csv'
+    BRONZE_PATH = f"{DATALAKE_PATH}/bronze/transaction_data"
+
+    df_source = (
+        spark
+        .read
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .option("ignoreLeadingWhiteSpace", "true")
+        .option("ignoreTrailingWhiteSpace", "true")
+        .csv(SOURCE_PATH)
+    )
+
+    if "_c0" in df_source.columns:
+        df_source = df_source.drop("_c0")
+
+    try:
+        df_bronze = DeltaTable.forPath(spark, BRONZE_PATH)
+        df_bronze.toDF().limit(1)
+
+        df_bronze.delete(f"transaction_date = '{data_processamento}'")
+
+        (
+            df_source
+            .write
+            .format('delta')
+            .mode('append')
+            .option('mergeSchema', 'true')
+            .save(BRONZE_PATH)
+        )
+
+    except Exception as e:
+        if 'DELTA_MISSING_DELTA_TABLE' in str(e):
+            (
+                df_source
+                .write
+                .format('delta')
+                .option('mergeSchema', 'true')
+                .mode('overwrite')
+                .save(BRONZE_PATH)
+            )
+        else:
+            raise e
+
+    return BRONZE_PATH
+
